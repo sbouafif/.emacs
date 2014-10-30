10fc87379f6252577d68760c7f56b9d90ff41e36
Slim Bouafif <slim.bouafif@gmail.com>
2014-10-30 03:06:33 +0100

    updated for with go-mode, whitespace, smart-tabs-mode

--- key-bindings.el
+++ key-bindings.el
@@ -3,11 +3,6 @@
 ;; Kill a word
 (global-set-key "\C-w" 'backward-kill-word)
 
-;; No tabs
-(setq-default indent-tabs-mode nil)
-(setq whitespace-style '(trailing tabs newline tab-mark newline-mark))
-(setq-default tab-width 2)
-
 ;; Line by line scrolling
 (setq scroll-step 1)
 
@@ -19,11 +14,6 @@
 
 ;; Goto-line short-cut key
 (global-set-key "\C-l" 'goto-line)
-
-;; 80 char column marker
-(require 'whitespace)
- (setq whitespace-style '(face empty tabs lines-tail trailing))
- (global-whitespace-mode t)
 
 ;; Fill column indicator
 (require 'fill-column-indicator)
